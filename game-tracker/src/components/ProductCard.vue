0a012e8991ab3088297df7e23e9463b51e38248b
fuuyuu <fuyukuogi@gmail.com>
2023-04-14 11:09:04 -0300

    ajustes no layout, inclusão dos filtros, separação do css do arquivo falta fazer o footer, tratamentos de erros e arrumar o layout

--- game-tracker/src/components/ProductCard.vue
+++ game-tracker/src/components/ProductCard.vue
@@ -18,6 +18,8 @@
 </template>
 
 <script>
+import "@/components/ProductCard.css";
+
 export default {
 	props: {
 		product: {
@@ -45,90 +47,3 @@ export default {
 	}
 };
 </script>
-
-<style scoped>
-	.card-container {
-		display: flex;
-		flex-wrap: wrap;
-		margin: 0 auto;
-	}
-
-	.card {
-		display: flex;
-		flex-direction: column;
-		margin-bottom: 10px;
-	}
-	.card-image img {
-		width: 380px;
-		height: 147px;
-		object-fit: cover;
-	}
-
-	.card-content {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		padding: 10px;
-		background-color: #0B1641
-	}
-
-	.card-title {
-		font-size: 24px;
-		text-align: left;
-		color: white;
-		font-family: 'Roboto';
-	}
-
-	.card-price {
-		font-size: 1.2rem;
-		font-weight: bold;
-		text-align: center;
-		color: white;
-		font-family: 'Roboto';
-		font-size: 18px;
-	}
-
-	.card-price-original {
-		font-size: 0.8rem;
-		text-align: center;
-		color: white;
-		text-decoration: line-through;
-		margin-top: 5px;
-		font-family: 'Roboto';
-		font-size: 12px;
-	}
-
-	.card-discount {
-		font-size: 0.8rem;
-		text-align: right;
-		color: white;
-		font-family: 'Roboto';
-	}
-
-	.card-details {
-		text-align: center;
-		padding: 10px;
-	}
-
-	button {
-		color: white;
-		height: 39px;
-		font-weight: bold;
-		border: none;
-		padding: 5px 10px;
-		cursor: pointer;
-		border-radius: 8px;
-		font-family: 'Roboto';
-		font-size: 18px;
-	}
-	button.details {
-		background-color: #C70160;
-		width : 116px;
-	}
-
-	button.discount {
-		background-color: #16857B;
-		width : 84px;
-
-	}
-</style>
